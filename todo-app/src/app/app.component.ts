ce14666092fcfc145fa12db1cad25d59e5d241a1
Kurt <kurtmilla1994@gmail.com>
2019-01-25 13:42:27 -0500

    Finished product. unable to complete. Version conflict

--- todo-app/src/app/app.component.ts
+++ todo-app/src/app/app.component.ts
@@ -3,8 +3,40 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'todo-app';
+  todoForm;
+  todo;
+
+  title = 'todo-app'; // DONT FORGET TO DECLARE PUBLIC
+  public todoArray = [];
+
+
+  public addTodo (value) {
+    this.todoArray.push (value);
+    console.log(this.todo);
+    console.log('Added Item');
+  }
+
+  public deleteItem(todo) {
+    for (let i: 0 ; i <= this.todoArray.length ; i++) {
+
+     if (todo === this.todoArray[i]) {
+
+      this.todoArray.splice(i, 1);
+     }
+    }
+    console.log('Delete Item');
+  }
+  public todoSubmit(value: any) {
+    if (value !== '') {
+      this.todoArray.push(value.todo);
+      } else {
+        alert('Field required **');
+      }
+     }
+
+
 }
+
